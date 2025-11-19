aa23404be8d10398f8dd18ab8ee3c9270cf6d21c
dandungjn <dandungjnjnjn@gmail.com>
2025-11-19 21:48:08 +0700

    refactor(home): use function from controller

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,16 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified', 'admin'])->name('dashboard');
-
-Route::get('/', function () {
-    return view('pages.home.index');
-})->name('home');
-
-Route::resource('/products', ProductController::class);
-Route::resource('/categories', CategoryController::class);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('/products', ProductController::class);
+    Route::resource('/categories', CategoryController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
